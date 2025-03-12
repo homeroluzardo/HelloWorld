434d2b359f7c5b52df2f3eb820eb90962ed8c1b7
homeroluzardo <homeroeluzardo@gmail.com>
2025-03-12 12:42:40 -0600

    Update Name

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "cloud.sun")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world! Homero")
+            Text("Hello, Homero Luzardo!")
                 .bold()
         }
         .padding()
