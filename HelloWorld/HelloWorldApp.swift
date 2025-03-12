af2bd8bb9e233fe0c9dce0830ea4257514661ab1
homeroluzardo <homeroeluzardo@gmail.com>
2025-03-11 20:09:55 -0600

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Homero Luzardo on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HelloWorldTests/HelloWorldTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldTests.swift
+//  HelloWorldTests
+//
+//  Created by Homero Luzardo on 3/11/25.
+//
+
+import Testing
+@testable import HelloWorld
+
+struct HelloWorldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
